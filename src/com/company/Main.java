332cf1422652d8eef8d0a9c716aad208179f4ef4
Karina <karinakokosova8@gmail.com>
2019-12-03 14:57:59 +0600

    exB

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Song s=new Song("Best part","H.E.R","R&B","You don't know bae,when you kiss me slowly it's a sweetest thing...");
-        FileWriter fileWriter=new FileWriter("Best part");
+        FileWriter fileWriter=new FileWriter("Best part.txt");
         fileWriter.write("Name:"+"\n");
         fileWriter.write("Singer :"+"\n");
         fileWriter.write("Genre :"+"\n");
